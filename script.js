957beb38fbe5635f8d1ada8f5c5961ddb03c60b5
Gail <gfike19@gmail.com>
2020-03-18 10:57:01 -0500

    attempted to work on studio

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    const fetchProm = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+
+    fetchProm.then( function(response) {
+        const container = document.getElementById("container");
+         
+     } );
+});
