118ccbe1b7ec3d8c2eba941bc15ca5f9313fd8cf
Gail Fike <gfike19@gmail.com>
2020-03-19 19:54:55 -0500

    started work on sorting list of astronauts by time spent in space

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 // TODO: add code here
 window.addEventListener("load", function () {
- fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-    response.json().then(function(json){
-         const container = document.getElementById("container");
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const container = document.getElementById("container");
 
-         for(let i = 0; i < json.length; i++) {
-             let div = `
+            let order = [json[0].id];
+            let id = json[0].id;
+            let hrs = json[id].hoursInSpace;
+            for (let i = 1; i < json.length; i++) {
+                let currentHrs = json[i].hoursInSpace;
+
+                if (currentHrs > hrs) {
+                    order.push(json[i].id)
+                }
+                else {
+                    order.unshift(json[i].id)
+                }
+            }
+
+            for(let each of order) {
+                let num = each -1;
+            // for (let i = 0; i < json.length; i++) {
+                let div = `
              <div class=astronaut>
-             <h3>${json[i].firstName + " " + json[i].lastName}</h3>         
+             <h3>${json[num].firstName + " " + json[num].lastName}</h3>         
                 <ul>
-                    <li>Skills: ${json[i].skills}</li>
+                    <li>Skills: ${json[num].skills}</li>
                     <li>Hours in Space: ${json[i].hoursInSpace}</li>
                     <li>Active: ${json[i].active}</li>
                 </ul>
                 <img class="avatar" src="${json[i].picture}"/>
             </div>
              `
-             container.innerHTML += div;
-         }
+                container.innerHTML += div;
+            }
+        })
+
     })
 
- })
-   
-
-            
-            // for(each of json) {
-        //         container.innerHTML = `
-        //     <div class="astronaut">
-        //         <ul>
-        //             <li>First Name: ${json[index].firstName}</li>
-        //             <li>Last Name: ${json.lastName}</li>
-        //             <li>Hours in Space: ${json.hoursInSpace}</li>
-        //             <li>Active: ${json.active}</li>
-        //             <img class="avatar" src=${json.picture}/>
-        //         </ul>
-        //     </div>
-        //  `;
-        // container.innerHTML += JSON.stringify(each);
-        // container.innerHTML += each;
-            // }
-        //  console.log(json);
-        });
+});
 
 
 
