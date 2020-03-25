076867db827dd169d250acdcc7f9c82e632ff9db
Gail Fike <gfike19@gmail.com>
2020-03-24 19:01:03 -0500

    cleaned up stuff, still not working

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 // TODO: add code here
 window.addEventListener("load", function () {
- fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-    response.json().then(function(json){
-         const container = document.getElementById("container");
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const container = document.getElementById("container");
 
-         for(let i = 0; i < json.length; i++) {
-             let div = `
+            for (let i = 0; i < json.length; i++) {
+                let div = `
              <div class=astronaut>
              <h3>${json[i].firstName + " " + json[i].lastName}</h3>         
                 <ul>
                     <li>Skills: ${json[i].skills}</li>
                     <li>Hours in Space: ${json[i].hoursInSpace}</li>
-                    <li>Active: ${json[i].active}</li>
-                </ul>
+                    `
+
+                if (json[i].active == true) {
+                    div += `<li color="green">Active: ${json[i].active}</li>`;
+                } else {
+                    div += `<li color="red">Active: ${json[i].active}</li>`;
+                }
+                div += `</ul>
                 <img class="avatar" src="${json[i].picture}"/>
             </div>
              `
-             container.innerHTML += div;
-         }
+                container.innerHTML += div;
+            }
+        })
+
     })
 
- })
-   
-
-            
-            // for(each of json) {
-        //         container.innerHTML = `
-        //     <div class="astronaut">
-        //         <ul>
-        //             <li>First Name: ${json[index].firstName}</li>
-        //             <li>Last Name: ${json.lastName}</li>
-        //             <li>Hours in Space: ${json.hoursInSpace}</li>
-        //             <li>Active: ${json.active}</li>
-        //             <img class="avatar" src=${json.picture}/>
-        //         </ul>
-        //     </div>
-        //  `;
-        // container.innerHTML += JSON.stringify(each);
-        // container.innerHTML += each;
-            // }
-        //  console.log(json);
-        });
+});
 
 
 
